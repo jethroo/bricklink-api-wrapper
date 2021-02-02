3d7ec370de18130f78563b269d673bf787548b26
Carsten Wirth <cwirth79@web.de>
2021-02-02 21:00:36 +0100

    adding first index version

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bricklink Api Wrapper
+
+This gem is a wrapper around the [Bricklink API](https://www.bricklink.com/v2/api/welcome.page)
+to make it easier to integrate it in ruby applications.
--- lib/bricklink_api_wrapper/user_inventory.rb
+++ lib/bricklink_api_wrapper/user_inventory.rb
@@ -20,5 +20,17 @@ module BricklinkApiWrapper
 
       new(json_body['data'])
     end
+
+    def self.index(params = { status: 'S'})
+      param_string = URI.encode_www_form(params).empty? ? '' : "?#{URI.encode_www_form(params)}"
+      net_http_response = Bricklink::Api.new.access_token.get("#{BASE_PATH}#{param_string}")
+      return unless net_http_response.code.to_i == 200
+
+      json_body = JSON.parse(net_http_response.body)
+
+      return unless json_body['meta']['code'] == 200
+
+       json_body['data'].map{ |inv_data| new(inv_data) }
+    end
   end
 end
